e7df0190eabe4da85d34c1924b19f9da1be418f3
2261-JOEL <joel80851@gmail.com>
2023-05-07 09:21:34 +0530

    build

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,7 @@
 const mongoose =require("mongoose");
 
 const DB="mongodb+srv://joel80851:joel80851@cluster0.ypxljdh.mongodb.net/mernstack?retryWrites=true&w=majority";
+// const DB=process.env.DATABASE
 
 mongoose.connect(DB,{
     
